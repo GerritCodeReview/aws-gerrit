c5dfab40cefe62c2e8f0f259a8e1391cc7cfc87e
Fabio Ponciroli <ponch78@gmail.com>
2020-03-31 19:43:19 +0200

    AWS Gerrit master-slave recipe
    CF templates and scripts to set up a Gerrit master
    instance replicating to a slave
    
    Feature: Issue 12516
    Change-Id: I46aa690e9fc2435d29259003eed47d672bb73d29

--- gerrit/entrypoint.sh
+++ gerrit/entrypoint.sh
@@ -3,12 +3,14 @@
 /tmp/setup_gerrit.py
 git config -f /var/gerrit/etc/gerrit.config gerrit.canonicalWebUrl "${CANONICAL_WEB_URL:-http://$HOSTNAME}"
 git config -f /var/gerrit/etc/gerrit.config httpd.listenUrl "${HTTPD_LISTEN_URL:-http://*:8080/}"
+git config -f /var/gerrit/etc/gerrit.config container.slave "${CONTAINER_SLAVE:-false}"
 
-if [ ! -d /var/gerrit/git/All-Projects.git ] || [ "$1" == "init" ]
+if [ $CONTAINER_SLAVE ]
 then
-  rm -fr /var/gerrit/plugins/*
-  echo "Initializing Gerrit site ..."
-  java -jar /var/gerrit/bin/gerrit.war init --batch --no-auto-start --install-all-plugins -d /var/gerrit
+  rm -fr /var/gerrit/plugins/replication.jar
+  java -jar /var/gerrit/bin/gerrit.war reindex --index groups
+else
+  java -jar /var/gerrit/bin/gerrit.war init --no-auto-start --batch --install-all-plugins -d /var/gerrit
 fi
 
 echo "Running Gerrit ..."
