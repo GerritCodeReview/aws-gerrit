d5dfd352d861e1d27d4f9cbb95b12bbbb5b030e9
Fabio Ponciroli <ponch78@gmail.com>
2020-04-13 15:44:04 +0200

    Add monitoring for Gerrit single master/single slave scenario
    Added Prometheus stack to monitor the master and
    slave instance.
    
    In a follow up change Grafana will be added for a better visualization
    of the metrics.
    
    Feature: Issue 12585
    Change-Id: I26e0dfb2c53e6e77dd52653cbd6530bee2e4b586

--- gerrit/entrypoint.sh
+++ gerrit/entrypoint.sh
@@ -7,12 +7,12 @@ git config -f /var/gerrit/etc/gerrit.config container.slave "${CONTAINER_SLAVE:-
 
 if [ ! -d /var/gerrit/git/All-Projects.git ] || [ "$1" == "init" ]
 then
-  rm -fr /var/gerrit/plugins/*
   echo "Initializing Gerrit site ..."
   java -jar /var/gerrit/bin/gerrit.war init --no-auto-start --batch --install-all-plugins -d /var/gerrit
   if [ $CONTAINER_SLAVE ]
   then
     echo "Cleanup slave git directory"
+    rm -f /var/gerrit/plugins/replication.jar
     rm -fr /var/gerrit/git/*
   fi
 fi
