7f6cdf0da62f094ce77a95eef703292ef0e76f08
Fabio Ponciroli <ponch78@gmail.com>
2020-06-01 21:00:02 +0200

    Reindex when spinning up second master
    Make sure reindexing is correctly triggered when
    spinning up the secondary master
    
    Change-Id: I821075ccb0d8149ea960ec7943e0caef20fcb492

--- gerrit/entrypoint.sh
+++ gerrit/entrypoint.sh
@@ -6,11 +6,14 @@ git config -f /var/gerrit/etc/gerrit.config httpd.listenUrl "${HTTPD_LISTEN_URL:
 git config -f /var/gerrit/etc/gerrit.config container.slave "${CONTAINER_SLAVE:-false}"
 
 if [ $CONTAINER_SLAVE ]; then
+  echo "Slave mode..."
   rm -fr /var/gerrit/plugins/replication.jar
   java -jar /var/gerrit/bin/gerrit.war reindex --index groups
 else
+  echo "Master mode (init phase)..."
   java -jar /var/gerrit/bin/gerrit.war init --no-auto-start --batch --install-all-plugins -d /var/gerrit
-  if [ ! -f /var/gerrit/index/gerrit_index.config ]; then
+  if [ $REINDEX_AT_STARTUP == "true" ]; then
+    echo "Master mode (reindex phase)..."
     java -jar /var/gerrit/bin/gerrit.war reindex -d /var/gerrit
   fi
 fi
