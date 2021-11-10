0d38672d6e8858fadf282808b42c8d2188c9ed2a
Antonio Barone <syntonyze@gmail.com>
2021-11-10 22:13:07 +0100

    WIP bootstrap from S3
    Change-Id: I5fc72ed20652f11b572ef2e4485d85843cf53310

--- gerrit/entrypoint.sh
+++ gerrit/entrypoint.sh
@@ -4,6 +4,7 @@
 git config -f /var/gerrit/etc/gerrit.config gerrit.canonicalWebUrl "${CANONICAL_WEB_URL:-http://$HOSTNAME}"
 git config -f /var/gerrit/etc/gerrit.config httpd.listenUrl "${HTTPD_LISTEN_URL:-http://*:8080/}"
 git config -f /var/gerrit/etc/gerrit.config container.replica "${CONTAINER_REPLICA:-false}"
+git config -f /var/gerrit/etc/gerrit.config gerrit.serverId "51623a7b-250d-4117-be3b-d12a353bafe1"
 
 if [ $CONTAINER_REPLICA ]; then
   echo "Replica mode..."
@@ -21,7 +22,16 @@ if [ $CONTAINER_REPLICA ]; then
      [ ! -d /var/gerrit/git/All-Users.git ] ||
      [ `git --git-dir=/var/gerrit/git/All-Projects.git show-ref | wc -l` -eq 0 ] ||
      [ `git --git-dir=/var/gerrit/git/All-Users.git show-ref | wc -l` -eq 0 ]; then
-     echo "Init phase..."
+
+     echo "[REPLICA] Installing All-Users from S3..."
+     aws s3 cp s3://gerritforge-git-bootstrap/3.3/All-Users.git.tar.gz /tmp
+     tar xzvf /tmp/All-Users.git.tar.gz -C /var/gerrit/git/
+
+     echo "[REPLICA] Installing All-Projects from S3..."
+     aws s3 cp s3://gerritforge-git-bootstrap/3.3/All-Projects.git.tar.gz /tmp
+     tar xzvf /tmp/All-Projects.git.tar.gz -C /var/gerrit/git/
+
+     echo "[REPLICA] Init phase..."
      java -jar /var/gerrit/bin/gerrit.war init --no-auto-start --batch --install-all-plugins -d /var/gerrit
   else
     echo "Reindexing phase..."
@@ -30,8 +40,17 @@ if [ $CONTAINER_REPLICA ]; then
   rm -fr /var/gerrit/plugins/replication.jar
 
 else
+  echo "[PRIMARY] Installing All-Users from S3..."
+  aws s3 cp s3://gerritforge-git-bootstrap/3.3/All-Users.git.tar.gz /tmp
+  tar xzvf /tmp/All-Users.git.tar.gz -C /var/gerrit/git/
+
+  echo "[PRIMARY] Installing All-Projects from S3..."
+  aws s3 cp s3://gerritforge-git-bootstrap/3.3/All-Projects.git.tar.gz /tmp
+  tar xzvf /tmp/All-Projects.git.tar.gz -C /var/gerrit/git/
+
   echo "Primary mode (init phase)..."
   java -jar /var/gerrit/bin/gerrit.war init --no-auto-start --batch --install-all-plugins -d /var/gerrit
+
   if [ $REINDEX_AT_STARTUP == "true" ]; then
     echo "Primary mode (reindex phase)..."
     java -jar /var/gerrit/bin/gerrit.war reindex -d /var/gerrit
