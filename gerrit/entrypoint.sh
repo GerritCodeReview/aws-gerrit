d51d45df91fe2607f69e55fdedb6b5833221505c
Antonio Barone <syntonyze@gmail.com>
2021-11-30 20:23:29 +0100

    Rename kafka-events to events-kafka
    kafka-events plugin has evolved as events-kafka. Point to the correct
    artifact and adjust the configuration accordingly.
    
    Bug: Issue 15346
    Change-Id: I453abc572273ac0c63874518f72201d9ccce81c9

--- gerrit/entrypoint.sh
+++ gerrit/entrypoint.sh
@@ -10,7 +10,7 @@ if [ $CONTAINER_REPLICA ]; then
 
   echo "Ensure primary specific plugins and libraries are not installed:"
   for jar in "lib/multi-site.jar" "plugins/multi-site.jar" "lib/replication.jar" \
-    "lib/events-broker.jar" "plugins/kafka-events.jar" "plugins/aws-dynamodb-refdb.jar" \
+    "lib/events-broker.jar" "plugins/events-kafka.jar" "plugins/aws-dynamodb-refdb.jar" \
     "plugins/websession-broker.jar" "plugins/high-availability.jar" "lib/high-availability.jar"
   do
     echo "rm -f /var/gerrit/$jar"
